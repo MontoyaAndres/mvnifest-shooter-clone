bcb23c9a9d022249cef3066962c05f47b0e6b64e
MontoyaAndres <andresmontoyafcb@gmail.com>
2022-02-03 15:40:58 -0500

    Initial commit

---
+++ test/mvnifest-shooter-clone.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MvnifestShooterClone from '../lib/mvnifest-shooter-clone-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mvnifest-shooter-clone-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MvnifestShooterClone.MvnifestShooterCloneStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
